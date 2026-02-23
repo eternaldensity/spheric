cade2e99fbc6deb5bb4e18d683e608c320ff0b7f
Daisy Snowdon <daisy@cybernetic.gay>
2026-02-23 22:56:47 +1100

    add missing colours

--- assets/js/systems/item_renderer.js
+++ assets/js/systems/item_renderer.js
@@ -1,21 +1,55 @@
 import * as THREE from "three";
 
 const ITEM_COLORS = {
+  // Raw ores
   iron_ore: 0xd4722a,
   copper_ore: 0x30c9a8,
-  iron_ingot: 0xaaaaaa,
-  copper_ingot: 0xdd8844,
   raw_quartz: 0xd4b8ff,
   titanium_ore: 0x556677,
-  titanium_ingot: 0x8899aa,
   crude_oil: 0x1a1a2e,
   raw_sulfur: 0xcccc22,
+  raw_uranium: 0x55ff55,
+
+  // Tier 0-1 processed
+  iron_ingot: 0xaaaaaa,
+  copper_ingot: 0xdd8844,
+  titanium_ingot: 0x8899aa,
   polycarbonate: 0x88bbdd,
   sulfur_compound: 0xaaaa44,
   wire: 0xdd8844,
   plate: 0xbbbbbb,
   circuit: 0x44ff88,
   frame: 0x667788,
+  quartz_crystal: 0xc8aaff,
+
+  // Tier 2-3 processed
+  motor: 0x99aacc,
+  cable: 0xcc6622,
+  reinforced_plate: 0xccccdd,
+  heat_sink: 0x4488cc,
+  heavy_frame: 0x556688,
+  advanced_circuit: 0x22cc66,
+  plastic_sheet: 0xddeeff,
+
+  // Tier 4-5 processed
+  computer: 0x33aa88,
+  motor_housing: 0x778899,
+  composite: 0x8899aa,
+  supercomputer: 0x22ddaa,
+  advanced_composite: 0x6677aa,
+
+  // Tier 6-8
+  enriched_uranium: 0x33ff33,
+  nuclear_cell: 0x44ff66,
+  containment_module: 0x8844cc,
+  dimensional_core: 0xaa44ff,
+  astral_lens: 0xcc88ff,
+  board_resonator: 0xff88cc,
+
+  // Creature / fuel
+  biofuel: 0x66aa44,
+  refined_fuel: 0x33aaaa,
+  creature_essence: 0xaa44cc,
   hiss_residue: 0xff2244,
 };
 
